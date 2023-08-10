a4bf2fa76af40d4e39daa67bd05a18a21b755b68
LSchuster <l.schuster1993@gmail.com>
2023-08-10 15:02:34 +0200

    Update InstallAgent.ps1

--- InstallAgent.ps1
+++ InstallAgent.ps1
@@ -5,18 +5,26 @@ param(
     $agentName
 )
 
+Write-Host "Set TLS Version"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Write-Host "Install Nuget"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 Write-Host "Install Az Module"
 Install-Module -Name Az -Repository PSGallery -Force
 
 Write-Host "Check if agent is installed"
 if (test-path "c:\agent") {
+    Write-Host "Remove c:\agent"
     Remove-Item -Path "c:\agent" -Force -Confirm:$false -Recurse
 }
 
+Write-Host "Create c:\agent"
 new-item -ItemType Directory -Force -Path "c:\agent"
 set-location "c:\agent"
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Write-Host "Download agent"
 $wr = Invoke-WebRequest https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases/latest
 $tag = ($wr | ConvertFrom-Json)[0].tag_name
 $tag = $tag.Substring(1)
@@ -28,10 +36,10 @@ write-host "Download and unpack agent files"
 Invoke-WebRequest $url -Out agent.zip
 Expand-Archive -Path agent.zip -DestinationPath $PWD
 
-write-host "Configure $agentName"
+write-host "Configure <AGENT_NAME>"
 .\config.cmd --unattended --url $url --auth pat --token $pat --pool $poolName --agent $agentName --acceptTeeEula --runAsService
 
-write-host "Start $agentName"
+write-host "Start <AGENT_NAME>"
 .\start
 
 
